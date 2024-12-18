97ec4b6debf14c4014b65316474cc9c37533bdb8
Mohsen Deilami <mohsen.deilami6570@gmail.com>
2024-12-18 06:35:54 +0100

    update

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,20 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { Navigate, Routes, Route } from "react-router-dom";
+import styles from "./App.module.css";
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
-    <div>
-     react
+    <div className={styles.container}>
+      react
+    {/* <Routes>
+          <Route path="/" element={<Navigate to="/products" replace />} />
+          <Route path="/products" element={<Products />} />
+          <Route path="/checkout" element={<Checkout />} />
+          <Route path="/products/:id" element={<Details />} />
+          <Route path="/*" element={<Notfound />} />
+        </Routes> */}
     </div>
   )
 }
